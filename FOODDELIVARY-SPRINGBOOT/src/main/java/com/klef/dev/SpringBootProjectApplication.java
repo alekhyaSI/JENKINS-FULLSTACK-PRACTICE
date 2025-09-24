4cf8b89d6533eab038c93f646911dbf31ec386b2
alekhyaSI <2300032935@kluniversity.in>
2025-09-24 15:50:36 +0530

    updated backend

--- FOODDELIVARY-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
+++ FOODDELIVARY-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootProjectApplication extends SpringBootServletInitializer {
+public class SpringBootProjectApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProjectApplication.class, args);
