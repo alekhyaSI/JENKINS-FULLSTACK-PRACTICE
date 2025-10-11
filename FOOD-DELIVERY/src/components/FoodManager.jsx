f6fe9095dec1db10d4a57aef70ffc670e24ed86a
alekhyaSI <alekhya.bandaru14@gmail.com>
2025-10-11 13:12:58 +0530

    updated project

--- FOOD-DELIVERY/src/components/FoodManager.jsx
+++ FOOD-DELIVERY/src/components/FoodManager.jsx
@@ -17,7 +17,7 @@ const FoodManager = () => {
   const [message, setMessage] = useState('');
   const [editMode, setEditMode] = useState(false);
 
-  const baseUrl = 'http://localhost:2030/foodapi';
+  const baseUrl = 'http://localhost:2030/springbootfoodapi';
 
 
   useEffect(() => {
